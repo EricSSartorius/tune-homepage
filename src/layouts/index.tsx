2e55ac293b9f19b39c0d6e954241ff1badd9cc92
David Yu <BOOST@Davids-MacBook-Air.local>
2020-06-29 01:36:16 +0900

    Updated

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <main>{children}</main>
-      <Footer />
+      <Footer siteTitle={data.site.siteMetadata.title} />
     </>
   )
 }
