262e5551d77817cb1238c16f7e0e98606a13c9e8
EricSSartorius <EricSSartorius@gmail.com>
2020-07-12 07:48:09 -0700

    Add intersection observer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+export const onClientEntry = async () => {
+  // IntersectionObserver polyfill for gatsby-image (Safari, IE)
+  if (typeof IntersectionObserver === `undefined`) {
+    await import(`intersection-observer`)
+  }
+}
