960271dc12a93407d1a7eddb668c1c790d2151c7
EricSSartorius <EricSSartorius@gmail.com>
2020-08-07 14:46:55 -0700

    Add favicon sizes

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,6 +48,64 @@ module.exports = {
         theme_color: `#e95424`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`,
+        icons: [
+          {
+            src: "icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          /* Below icons from realfavicongenerator.net */
+          {
+            src: `icons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/apple-touch-icon.png`,
+            sizes: `180x180`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/favicon-32x32.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: `icons/favicon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+        ],
       },
     },
     {
