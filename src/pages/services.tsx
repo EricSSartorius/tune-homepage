1e784c68adf7075273a36ee4e935b2948d6e15aa
EricSSartorius <EricSSartorius@gmail.com>
2020-08-17 09:12:17 -0700

    WIP starts translation

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -4,47 +4,59 @@ import styled from "styled-components"
 import Img from "gatsby-image"
 import SEO from "../components/seo"
 import { Wrapper, Cols } from "../styles"
+import { useLanguage } from "../global/language"
+import { english, thai } from "../translation/services.yml"
 
-const ServicesPage: FC = ({ data }) => (
-  <>
-    <SEO title="Services" />
-    <HeroImg fluid={data.heroImg.childImageSharp.fluid} />
-    <Wrapper>
-      <section>
-        <Cols isReverse>
-          <h1>Breakdown of Architecture and Interior design services</h1>
-          <div>
-            {services.map(({ title, items }) => (
-              <Fragment key={title}>
-                <h3>{title}</h3>
-                {items && (
-                  <ul>
-                    {items.map(item => (
-                      <li key={item}>{item}</li>
-                    ))}
-                  </ul>
-                )}
-              </Fragment>
-            ))}
-            <p className="margins">
-              <em>*included: Structural engineer</em>
-            </p>
-            <p>
-              <em>
-                **not included: Permit Drawing Architect fee, Electrical
-                engineer, Sanitary engineer, Land Survey, Landscape Design
-              </em>
-            </p>
-          </div>
-        </Cols>
-      </section>
-    </Wrapper>
-  </>
-)
+const ServicesPage: FC = ({ data }) => {
+  const { lang } = useLanguage()
+
+  const services = lang === "th" ? thaiServices : englishServices
+
+  const currentLanguage = lang === "th" ? thai : english
+
+  return (
+    <>
+      <SEO title="Services" />
+      <HeroImg fluid={data.heroImg.childImageSharp.fluid} />
+      <Wrapper>
+        <section>
+          <Cols isReverse>
+            <h1>{currentLanguage.title}</h1>
+            <div>
+              {services.map(({ title, items }) => (
+                <Fragment key={title}>
+                  <h3>{title}</h3>
+                  {items && (
+                    <ul>
+                      {items.map(item => (
+                        <li key={item}>{item}</li>
+                      ))}
+                    </ul>
+                  )}
+                </Fragment>
+              ))}
+              <p className="margins">
+                <em>*included: Structural engineer</em>
+              </p>
+              <p>
+                <em>
+                  {lang === "th"
+                    ? "poo"
+                    : `**not included: Permit Drawing Architect fee, Electrical
+                  engineer, Sanitary engineer, Land Survey, Landscape Design`}
+                </em>
+              </p>
+            </div>
+          </Cols>
+        </section>
+      </Wrapper>
+    </>
+  )
+}
 
 export default ServicesPage
 
-const services = [
+const englishServices = [
   {
     title: "Conceptual Design",
     items: ["Layout (basic schematic drawing)", "3 or 4 Conceptual Renderings"],
@@ -60,6 +72,41 @@ const services = [
   {
     title:
       "Construction Drawing 1 - Permit Drawing (Architecture) and Tender Drawing",
+    items: [
+      "Tender Package including (Interior Plans, Elevations, Sections, Structural Section)",
+      "Permit Drawing",
+    ],
+  },
+  {
+    title: "Construction Drawing 2 - Tender Drawing",
+    items: [
+      "Complete drawing including finishing (floor, ceiling), and lighting plan",
+      "Complete drawing including finishing (wall), and lighting elevation",
+      "Loose Furniture Recommendations",
+    ],
+  },
+  {
+    title: "Construction Period",
+  },
+  {
+    title: "Project Handover",
+    items: [
+      "Assist the Client to oversee the quality of work done by the Contractor",
+    ],
+  },
+]
+
+const thaiServices = [
+  {
+    title: "Cpoo",
+    items: ["Cpoo"],
+  },
+  {
+    title: "Cpoo",
+    items: ["Cpoo", "Cpoo"],
+  },
+  {
+    title: "Cpoo",
     items: [
       "Tender Package including (Interior Plans, Elevations, Sections, Structural Section)",
       "Permit Drawing",
