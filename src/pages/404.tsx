cdb90c756522fa57c827364f43ac1a22e323e889
EricSSartorius <EricSSartorius@gmail.com>
2020-08-29 11:09:11 -0700

    Update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,37 @@
 import React from "react"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+import Img from "gatsby-image"
 import SEO from "../components/seo"
+import { Wrapper } from "../styles"
 
-const NotFoundPage: React.FC = () => (
+const NotFoundPage: React.FC = ({ data }) => (
   <>
     <SEO title="404" />
-    <h1>404: Not Found</h1>
+    <HeroImg fluid={data.heroImg.childImageSharp.fluid} />
+
+    <Wrapper>
+      <section className="container">
+        <h1>404: Not Found</h1>
+      </section>
+    </Wrapper>
   </>
 )
 
 export default NotFoundPage
+
+const HeroImg = styled(Img)`
+  max-height: 30rem;
+`
+
+export const query = graphql`
+  query {
+    heroImg: file(relativePath: { eq: "hero/honeyful-hero.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1440) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
