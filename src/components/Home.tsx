48c0c38cbca4a2b83c1aad9d9050c2b51b963628
EricSSartorius <EricSSartorius@gmail.com>
2020-10-07 19:54:15 -0700

    seo title update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -48,7 +48,7 @@ const Home = ({ data, lang }) => {
 
   return (
     <>
-      <SEO lang={lang} />
+      <SEO lang={lang} title="Home" />
       <Img fluid={heroImg.childImageSharp.fluid} style={{ height: "100vh" }} />
 
       <Wrapper>
